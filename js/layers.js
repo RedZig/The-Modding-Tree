c8bc6ae02f5458c75a8a1e85fdd4787f1f62af1f
RedZig <Red.Zig@users.noreply.github.com>
2020-12-25 13:18:27 -0600

    Test

--- js/layers.js
+++ js/layers.js
@@ -26,10 +26,10 @@ addLayer("p", {
     ],
     layerShown(){return true}
 })
-addLayer("p", {
+addLayer("T", {
     name: "Test", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
